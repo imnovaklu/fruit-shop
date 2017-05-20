0ffc1dbbbde48578b5a9ad35332c086b8f6fe577
english10010 <564275140@qq.com>
2017-05-20 17:17:09 -0400

    add delete functionality

--- src/components/cartItem.js
+++ src/components/cartItem.js
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import Store from '../store';
 import './cartItem.less';
 
 class CartItem extends Component{
     constructor(props){
         super(props);
+    }
+
+    _delete(e){
+        Store.dispatch({
+            type: 'DELETE_FROM_CART',
+            payload: {
+                name: e.target.getAttribute('data-name')
+            }
+        })
     }
 
     render(){
@@ -19,7 +29,7 @@ class CartItem extends Component{
                 </div>
                 <div>
                     <span>{`@ $${price}each = $${price*quantity}`}</span>
-                    <span title="delete" className="pull-right text-delete">Delete</span>
+                    <span title="delete" className="pull-right text-delete" data-name={name} onClick={this._delete}>Delete</span>
                 </div>
             </div>
         )
